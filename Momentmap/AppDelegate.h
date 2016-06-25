623070f331c009c291284452a3c866728128be7d
Kristina Covington <teenyton@yahoo.com>
2016-06-25 14:03:33 -0700

    Initial Commit

---
+++ Momentmap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Momentmap
+//
+//  Created by Kristina Covington on 6/25/16.
+//  Copyright © 2016 Kristina Covington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Momentmap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Momentmap
+//
+//  Created by Kristina Covington on 6/25/16.
+//  Copyright © 2016 Kristina Covington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
